f1e4e8e0471a6a785fd89f05430703f1071e704d
MintCollie <dragoncool57@yahoo.com>
2020-10-14 15:13:10 -0500

    added new mainpage.cs

---
+++ dotnettut/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+namespace dotnettut
+{
+    public class MainPage : ContentPage
+    {
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
+        public MainPage()
+        {
+            this.Padding = new Thickness(20, 20, 20, 20);
+
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 15
+            };
+
+            panel.Children.Add(new Label
+            {
+                Text = "Enter a phoneword:",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            });
+        }
+    }
+}
